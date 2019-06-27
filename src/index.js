7e09bde8fcad98d8ae7d084b41d6c0d121e3ce9a
filipe custodio <filipe.l.custodio@gmail.com>
2019-06-28 00:37:14 +0100

    Introducing React state to class components

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render () {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+  }
+
+  render() {
     window.navigator.geolocation.getCurrentPosition(
       position => console.log(position),
       err => console.log(err)
     );
-    return <div>Latitude: </div>
+    return <div>Latitude: {this.state.lat} </div>
   }
 }
 
